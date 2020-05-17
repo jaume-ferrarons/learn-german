e8f936cd115cb1f606f4561c2af3b1b1b69fef1e
Jaume <ckroxigor@gmail.com>
2020-05-17 17:45:22 +0200

    Build

---
+++ build/precache-manifest.00c80ca5401928bc43fb7277c12e3cb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1757a61be3ea7713bbda4fce951bf50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2d2e565b11c2b8a3b08",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f9aee17413a1b6384a0",
+    "url": "/static/js/2.f779c42d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f779c42d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d2e565b11c2b8a3b08",
+    "url": "/static/js/main.7c93383b.chunk.js"
+  },
+  {
+    "revision": "451fccfb28e6578fee47",
+    "url": "/static/js/runtime-main.ba78dc76.js"
+  }
+]);
